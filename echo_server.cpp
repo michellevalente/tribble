f16c98ea3f58a122cab7127add38550e4894dbf0
Marcio Paiva <marcioapf@gmail.com>
2015-03-30 00:34:45 -0400

    Sample use case (echoserver)
    This should guide our socket implementation.

---
+++ echo_server.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "tribble.h"
+
+using namespace std;
+using namespace tribble;
+
+int main() {
+	Socket wsock(8080);
+
+	while (true) {
+		Socket sock = wsock.acpt();
+
+		while(true) {
+			error_code error;
+			string rcvd = sock.receive(1024, error);
+			// if (error = error::eof)
+				// break;
+			sock.send_all(rcvd);
+		}
+	}
+
+	return 0;
+}
