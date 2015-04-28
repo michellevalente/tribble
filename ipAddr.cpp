4d64c244183a626f301a53ffedc69167cd79fd6f
Michelle Valente <ma3360@columbia.edu>
2015-04-28 16:50:23 -0400

    Sample program testing HostAddr functions.

---
+++ ipAddr.cpp
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string>
+#include <iostream>
+#include "HostAddr.hpp"
+
+
+/* Sample program to test if it is a valid ip
+ * and calculate broadcast ip. 				
+ */
+int main()
+{
+	string ip, mask, broadcastip;
+	int family;
+	
+	cout << "Ip address: ";
+	cin >> ip;
+
+	HostAddr ipAddr(ip);
+
+	family = ipAddr.family(); 
+
+	if(family == 4)
+		cout << ip << " is a valid IPV4 address." << endl;
+	else
+		cout << ip << " is a valid IPV4 address." << endl;
+
+	cout << "Enter a mask to calculate the broadcast IP: ";
+	cin >> mask;
+
+	broadcastip = ipAddr.generateBroadcast(mask);
+	cout << "Broadcast IP: " << broadcastip << endl;
+}
