0abb4567af35c7e4518b7484640dcbc7033ddc9d
Michelle Valente <ma3360@columbia.edu>
2015-04-28 16:47:41 -0400

    Sample files to test UDP connection.

---
+++ echo_client_udp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "socket.hpp"
+
+int main(int argc, char** argv)
+{
+	DatagramSocket sock;
+	string ip = string(argv[1]);
+	string port_str = string(argv[2]);
+	int port = stoi(port_str);
+	HostAddr server(ip, port);
+	char message[1024];
+	for(;;)
+	{
+		cin >> message;
+		sock.sendTo(string(message), server);
+		cout << "msg: " << sock.receiveFrom(1024, server) << endl;
+	}
+
+}
---
+++ echo_server_udp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "socket.hpp"
+
+int main()
+{
+   DatagramSocket sock(5000);
+   HostAddr client;
+   string message;
+   
+   for(;;)
+   {
+      message = sock.receiveFrom(1024, client);
+      cout << "msg: " << message << endl;
+      sock.sendTo(message, client);
+   }
+
+   return 0;
+}
