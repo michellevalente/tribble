105f3e86e6ff558560c37a548191def0c89e64b1
Renato Nascimento <rhnasc@gmail.com>
2015-04-29 18:56:39 -0400

    Added different file with operators << >>
    You might be netcating with \n instead of ctrl-D. The \n will fail for the regular echo_server.cpp if you send any message over the buffer size.

---
+++ samples/echo_server_operators.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "tribble.h"
+
+//using namespace std;
+//using namespace tribble;
+
+int main() {
+    StreamSocket wsock(5000);
+
+    while (true) {
+        StreamSocket sock = wsock.accept();
+
+        cout<<"  Connection established with "<<sock.getPeerAddress()<<endl; 
+
+        string rcvd;
+
+        while(sock >> rcvd) 
+            sock << "Starting to echo\n" << rcvd << "Finishing echo\n";
+    }
+
+    return 0;
+}
