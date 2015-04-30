66c001d0881e63ede2ecfaa3b61afcca166e1ed6
Michelle Valente <ma3360@columbia.edu>
2015-04-30 11:09:49 -0400

    Date server/client sample.

---
+++ samples/date_client.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "StreamSocket.hpp"
+
+using namespace std;
+
+int main()
+{
+	string serverAddr;
+	int port;
+
+	cout << "Server address: ";
+	cin >> serverAddr;
+
+	cout << "Port number: ";
+	cin >> port;
+
+	HostAddr server(serverAddr, port);
+	StreamSocket socket;
+
+	socket.connect(server);
+
+	cout << socket.receive(1024) << endl;;
+}
---
+++ samples/date_server.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include "StreamSocket.hpp"
+
+using namespace std;
+
+int main()
+{
+	int port;
+	time_t t;
+	struct tm * t_info;
+	string date;
+	char buffer[100];
+
+	time(&t);
+	t_info = localtime(&t);
+
+	strftime(buffer,100,"%d-%m-%Y %I:%M:%S",t_info);
+
+	date = string(buffer);
+
+	cout << "Port number: ";
+	cin >> port;
+
+	StreamSocket socket(port);
+
+	for(;;)
+	{
+		StreamSocket clientSocket = socket.accept();
+		clientSocket.send_all(date);
+	}
+}
